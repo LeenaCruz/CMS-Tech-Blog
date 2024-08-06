1ef4e963b88d2d46cb5995fd2e1014c6106bfae2
Leena <leenacruz@gmail.com>
2024-08-06 13:06:28 -0700

    Fix bug on submit post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
     // Set up sessions with a 'loggedIn' variable set to `true`
     req.session.save(() => {
       req.session.loggedIn = true;
-
+req.session.user_id = dbUserData.id;
       res.status(200).json(dbUserData);
     });
   } catch (err) {
