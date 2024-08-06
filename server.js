a70ff820f92b96c78b4a122ab554a0291bffebe3
Leena <leenacruz@gmail.com>
2024-08-06 12:08:21 -0700

    Logout works

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const session =require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -12,8 +13,17 @@ const PORT = process.env.PORT || 3001;
 // Set up sessions
 const sess = {
   secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 60*60*1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
